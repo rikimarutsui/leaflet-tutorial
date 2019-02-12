3982b9b92ecb409bed2d3a4bc7c13cf98900e918
Ricky Tsui <tktsui@hko.hksarg>
2019-02-12 17:18:31 +0800

    Second Handle Method

---
+++ readjson2.php
@@ -0,0 +1,38 @@
+<?php
+function readJSONList($path = './'){
+	$files = scandir($path);
+	for($i = 0; $i < sizeof($files); $i++){
+		$file = $files[$i];
+		if(strpos($file, '.json') !== false){
+
+		}else{
+			unset($files[$i]);
+		}
+	}
+	return $files;
+}
+
+function readJSONFile($files, $path = './'){
+	$jsons = '[';
+	foreach($files as $file){
+		$json = file_get_contents($path . $file);
+		$json_s = json_decode($json);
+		$json = json_encode(array(
+			'time' => $json_s->time,
+			'Lat' => $json_s->df17->Lat,
+			'Lng' => $json_s->df17->Lon,
+			'windSpd' => $json_s->windSpd_kt,
+			'windDir' => $json_s->windDir_deg,
+			'temperature' => $json_s->temperature_K,
+			'createdAt' => $json_s->createdAt
+		));
+		$jsons = $jsons . $json . ',';
+	}
+	$jsons = substr($jsons, 0, -1) . ']';
+	return $jsons;
+}
+
+$files = readJSONList('./jsondata-sampleset/');
+$jsons = readJSONFile($files, './jsondata-sampleset/');
+echo $jsons;
+?>
